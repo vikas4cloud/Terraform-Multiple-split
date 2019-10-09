9e13c6dce269fc9aa7dc9bd9c7099f53f202653c
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-09 11:35:13 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,10 +4,10 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "resource_group_name" {}
 variable "location" {}
-variable "snapshot_name" {}
+
 variable "source_uri" {
   description = "source_uri of VM_OS Disk"
   type        = list(string)
   
 }
-variable "count1" {}
+
