ec226456e4fb77cd023a7b1acd7c39c66978ce17
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-09 11:26:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "pwc-test" {
 resource "azurerm_snapshot" "pwc-test" {
 
  
-  count = "${var.count}"
+  count = "${var.count1}"
  
   name  = "test-snapshot3_${count.index}"
 
