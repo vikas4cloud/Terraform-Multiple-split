d119a31c88c80684cab55eb6e7a90c45ccf06736
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-10 01:05:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,25 @@ resource "azurerm_snapshot" "pwc-test" {
  
    #count = "2"
  
+ dynamic "setting" {
+    for_each = var.source_uri
+    content {
+      #namespace = "aws:elasticbeanstalk:application:environment"
+      name      = setting.key
+      value     = setting.value
+     
+     
+  location            = "${azurerm_resource_group.pwc-test.location}"
+  
+  resource_group_name = "${azurerm_resource_group.pwc-test.name}"
+
+  create_option       = "Copy"
+
+  #source_uri          =  var.source_uri[count.index]
+ source_uri          =  var.value
+    }
+  }
+ 
       key   = "${element(split(",", var.source_uri), 1)}"
 
   location            = "${azurerm_resource_group.pwc-test.location}"
