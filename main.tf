4c969cb9bb362cc577343f8c8bd8780d63a2b7a4
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-10 11:35:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,23 @@
 provider "azurerm" {
-
- subscription_id = "${var.subscription_id}"
+  subscription_id = "${var.subscription_id}"
   tenant_id       = "${var.tenant_id}"
   client_id       =  "${var.client_id}"
   client_secret   =  "${var.client_secret}"
- 
-  
-  }
+ }
 
 resource "azurerm_resource_group" "pwc-test" {
-
- name     = "${var.resource_group_name}"
- location = "${var.location}"
-
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
 }
 
- 
 
-
-resource "azurerm_snapshot" "pwc-test" {
-
- 
-   count = length(var.source_uri)
- 
+resource "azurerm_snapshot" "pwc-test"  { 
+  count = length(var.source_uri)
   name  = "test-snapshot3_${count.index}"
-
-  location            = "eastus"
-
-  resource_group_name = "pwc-test"
-
-  create_option       = "Copy"
-
- # source_uri          = "${var.source_uri}"
-  source_uri          =  var.source_uri[count.index]
+  location = "${azurerm_resource_group.pwc-test.location}"
+  resource_group_name = "${azurerm_resource_group.pwc-test.name}"
+  create_option = "Copy"
+  source_uri =  var.source_uri[count.index]
 }
 
 
