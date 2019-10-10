be1a57cc16e727b17c7ec5236ae7e2f6152d2806
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-10 04:43:42 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "resource_group_name" {}
 variable "location" {}
 
 variable "source_uri" {
-  description = "source_uri of VM_OS Disk"
+  #description = "source_uri of VM_OS Disk"
   type        = list(string)
   #default     = ["/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/PWC-TEST/providers/Microsoft.Compute/disks/VM1_OsDisk_1_364ab25d96064260b6e89bb2ace19623", "/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/PWC-TEST/providers/Microsoft.Compute/disks/Vm2_disk1_d1269e293e37420f8c67c2c306e092e9"]
 }
