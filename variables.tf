d8d3335e3f5dae95262be3b024ef92bc63942deb
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-10 11:28:59 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,18 +4,8 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "resource_group_name" {}
 variable "location" {}
-
-
 variable "source_uri" {
   type = list(string)
-
-  
-#default     = []
 }
-#variable "source_uri" { type = "list" }
 
-#variable "source_uri" {
- # type        = list(string)
-  #default     = []
-#}
 
